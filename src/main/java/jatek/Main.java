2919ca707e233fec3b2448e4ff30d8649af61b7d
Berkes Márton <Marton.Berkes@idomsoft.hu>
2022-07-15 19:21:01 +0200

    websocket eljut, de aztán javax.websocket.DeploymentException: The HTTP request to initiate the WebSocket connection

--- src/main/java/jatek/Main.java
+++ src/main/java/jatek/Main.java
@@ -1,7 +1,8 @@
 package jatek;
 
 
-import jatek.tetrisbackend.gamer.WebsocketClientEndpoint;
+
+import ch.qos.logback.core.net.server.Client;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -9,6 +10,10 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.websocket.*;
 
 public class Main {
@@ -17,16 +22,26 @@ public class Main {
     public static void main(String[] args)  {
         System.out.println("Hajrá MacskaMarcik!");
 
-        try{
-            final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("ws://tetris-backend-websocket.platform-dev.idomsoft.hu/tetris"));
+//        try{
+//            final WebsocketClientEndpoint clientEndPoint = new WebsocketClientEndpoint(new URI("ws://tetris-backend-websocket.platform-dev.idomsoft.hu/tetris"));
+//
+////            Socket s=new Socket("tetris-backend-websocket.platform-dev.idomsoft.hu",9080);
+//            for (;;) {
+//                DataInputStream din = new DataInputStream(s.getInputStream());
+//                System.out.println(din.readUTF());
+//                din.close();
+//                s.close();
+//            }
+//        }catch(Exception e){System.out.println(e);}
 
-//            Socket s=new Socket("tetris-backend-websocket.platform-dev.idomsoft.hu",9080);
-            for (;;) {
-                DataInputStream din = new DataInputStream(s.getInputStream());
-                System.out.println(din.readUTF());
-                din.close();
-                s.close();
-            }
-        }catch(Exception e){System.out.println(e);}
+        try {
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            String uri = "ws://tetris-backend-websocket.platform-dev.idomsoft.hu/tetris:9080/";
+            System.out.println("Connecting to " + uri);
+            container.connectToServer(MyClientEndpoint.class, URI.create(uri));
+        } catch (DeploymentException | IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 }
